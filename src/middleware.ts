4f2aa6cfd93284534710357176fab757fdfdb393
Aryan kadam <113440600+aryyan0701@users.noreply.github.com>
2024-11-15 17:01:38 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,29 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt"; // Import to check authentication token
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
 
-export { default } from "next-auth/middleware";
+export const config = {
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/'],
+};
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // If the user is logged in and they try to access sign-in, sign-up, or home, redirect them to the dashboard
-  if (token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname === '/')) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  // If the user is not logged in and they try to access the dashboard, redirect them to sign-in
   if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    return NextResponse.redirect(new URL('/sign-in', request.url));
   }
-}
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*"], // Add slashes to paths
-};
+  return NextResponse.next();
+}

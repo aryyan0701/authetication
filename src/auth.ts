092edcc25de40ba4fc0de4effc58df8afe617ba5
Aryan kadam <aryankadam071@gmail.com>
2024-12-18 12:31:32 +0530

    sign-in auth changes

--- src/auth.ts
+++ src/auth.ts
@@ -7,10 +7,10 @@ import bcrypt from 'bcryptjs';
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         CredentialsProvider({
-            id: "Credentials",
+            id: "credentials",
             name: "Credentials",
             credentials: {
-                email: { label: "Email", type: "text" },
+                identifier: { label: "Email or Username", type: "text" },
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials: any): Promise<any> {
@@ -43,26 +43,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     session: {
         strategy: "jwt" 
     },
-     callbacks: {
-    async jwt({ token, user }) {
-    console.log("JWT Callback - Token before:", token);
-      if (user) {
-        token.id = user.id
-        token.email = user.email;
-      }
-      console.log("JWT Callback - Token after:", token);
-      return token
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.email = user.email;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user._id = token.id as string;
+                session.user.email = token.email as string;
+            }
+            return session;
+        },
     },
-    async session({ session, token }) {
-        console.log("Session Callback - Token:", token);
-      if (session.user) {
-        session.user._id = token.id as string
-        session.user.email = token.email as string
-    }
-    console.log("Session Callback - Session:", session);
-      return session
-    },
-  },
     pages: {
         signIn: "/sign-in" 
     },
